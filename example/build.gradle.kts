5222477b1e36d4b623690df268d7aec2a29ddbe6
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-06 23:35:40 +0200

    Nest example project in multi-project build

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,24 +1,7 @@
 plugins {
-    id("org.metaborg.convention.java")
-    id("org.metaborg.convention.maven-publish")
+    id("org.metaborg.convention.root-project")
 }
 
-group = "org.metaborg"
-description = "A Metaborg Gradle convention plugin example project."
-
-javaConvention {
-    javaVersion.set(JavaLanguageVersion.of(17))
-}
-
-mavenPublishConvention {
-    repoOwner.set("metaborg")
-    repoName.set("convention-plugin-example")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
-    }
+rootProjectConvention {
+    taskNameSuffix.set("All")
 }
