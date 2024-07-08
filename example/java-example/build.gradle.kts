f42750a08528932eb95b5bb7ea6dbbd626f114cf
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-08 16:10:16 +0200

    Add comments

--- example/java-example/build.gradle.kts
+++ example/java-example/build.gradle.kts
@@ -8,15 +8,17 @@ group = "org.metaborg"
 description = "A Metaborg Gradle convention plugin example project."
 
 javaConvention {
-    javaVersion.set(JavaLanguageVersion.of(17))
+    javaVersion.set(JavaLanguageVersion.of(17))     // Optional
 }
 
 mavenPublishConvention {
+    // Required
     repoOwner.set("metaborg")
     repoName.set("convention-plugin-example")
 }
 
 publishing {
+    // Required for the maven-publish convention to publish anything
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
