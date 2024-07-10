bdc60d969f8443b91b815bd4e0564c5980cf352c
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-10 13:21:23 +0200

    Update version catalog

--- convention-plugin/settings.gradle.kts
+++ convention-plugin/settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("org.metaborg.spoofax3:catalog:0.3.11")
+            from("org.metaborg.spoofax3:catalog:0.4.0")
         }
     }
 }
