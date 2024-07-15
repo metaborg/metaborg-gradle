167bfbcb3745337f53416ee853dea3db8c9ac3aa
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-15 11:56:46 +0200

    Allow configuring Gradle Enterprise plugin to fail

--- convention-plugin/src/main/kotlin/org/metaborg/convention/SettingsConventionPlugin.kt
+++ convention-plugin/src/main/kotlin/org/metaborg/convention/SettingsConventionPlugin.kt
@@ -5,10 +5,14 @@ package org.metaborg.convention
 import com.gradle.develocity.agent.gradle.DevelocityConfiguration
 import com.gradle.enterprise.gradleplugin.GradleEnterpriseExtension
 import org.gradle.api.Plugin
+import org.gradle.api.UnknownDomainObjectException
 import org.gradle.api.initialization.Settings
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.maven
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
+private val LOG: Logger = LoggerFactory.getLogger("FooPlugin")
 
 /**
  * Configures a Gradle build.
@@ -47,31 +51,43 @@ class SettingsConventionPlugin: Plugin<Settings> {
         // Apply the Foojay plugin
         plugins.apply("org.gradle.toolchains.foojay-resolver-convention")
 
-        val isCI = System.getenv("CI").isNullOrEmpty()
         gradle.settingsEvaluated {
             if (plugins.hasPlugin("com.gradle.build-scan") || plugins.hasPlugin("com.gradle.enterprise")) {
-                // Configure the Gradle Enterprise plugin if it's configured
-                settings.extensions.getByType(GradleEnterpriseExtension::class.java).apply {
-                    buildScan {
-                        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-                        termsOfServiceAgree = "yes"
-                        publishAlwaysIf(isCI)
-                        if (isCI) tag("CI")
-                    }
-                }
+                tryConfigureGradleEnterprisePlugin()
             } else {
-                // Otherwise, apply and configure the Gradle Develocity plugin and configure it
                 plugins.apply("com.gradle.develocity")
-                settings.extensions.getByType(DevelocityConfiguration::class.java).apply {
-                    buildScan {
-                        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
-                        termsOfUseAgree.set("yes")
-                        publishing.onlyIf { isCI }
-                        if (isCI) tag("CI")
-                        capture {
-                            fileFingerprints.set(true)
-                        }
-                    }
+                configureDevelocityPlugin()
+            }
+        }
+    }
+
+    private fun Settings.tryConfigureGradleEnterprisePlugin() {
+        val isCI = System.getenv("CI").isNullOrEmpty()
+        try {
+            settings.extensions.getByType(GradleEnterpriseExtension::class.java).apply {
+                buildScan {
+                    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+                    termsOfServiceAgree = "yes"
+                    publishAlwaysIf(isCI)
+                    if (isCI) tag("CI")
+                }
+            }
+        } catch (e: UnknownDomainObjectException) {
+            LOG.info("Could not configure Gradle Enterprise plugin")
+            // Ignored
+        }
+    }
+
+    private fun Settings.configureDevelocityPlugin() {
+        val isCI = System.getenv("CI").isNullOrEmpty()
+        settings.extensions.getByType(DevelocityConfiguration::class.java).apply {
+            buildScan {
+                termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+                termsOfUseAgree.set("yes")
+                publishing.onlyIf { isCI }
+                if (isCI) tag("CI")
+                capture {
+                    fileFingerprints.set(true)
                 }
             }
         }
