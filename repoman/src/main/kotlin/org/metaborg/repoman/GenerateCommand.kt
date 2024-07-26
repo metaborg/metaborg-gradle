1b93705aec8b5dd164c4217c196505dd1f3b80ac
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-26 11:26:17 +0200

    Generate Gradle wrapper

--- repoman/src/main/kotlin/org/metaborg/repoman/GenerateCommand.kt
+++ repoman/src/main/kotlin/org/metaborg/repoman/GenerateCommand.kt
@@ -3,6 +3,8 @@ package org.metaborg.repoman
 import com.charleskorn.kaml.Yaml
 import com.charleskorn.kaml.decodeFromStream
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
 import gg.jte.ContentType
@@ -10,9 +12,9 @@ import gg.jte.TemplateEngine
 import gg.jte.output.FileOutput
 import gg.jte.resolve.ResourceCodeResolver
 import org.metaborg.repoman.meta.RepoMetadata
+import java.io.IOException
 import java.nio.file.Path
-import kotlin.io.path.exists
-import kotlin.io.path.inputStream
+import kotlin.io.path.*
 
 /**
  * Generates or updates the files in a repository, including:
@@ -38,6 +40,15 @@ object GenerateCommand: CliktCommand(
     val repoDir: Path? by option("-r", "--repo", help = "The directory with the repository")
         .path(canBeFile = false, canBeDir = true, mustExist = true)
 
+    /** The Gradle binary to invoke. */
+    val gradleBin: String? by option("--gradle-bin", help = "The Gradle binary to invoke")
+        .default("gradle")
+
+    /** Whether to force updating files even if they exist. Use this to just update all files and manually
+     * use version control to sort out what to actually update. */
+    val forceUpdate: Boolean by option("-f", "--force-update", help = "Force updating files even if they exist")
+        .flag(default = false)
+
 
     override fun run() {
         val repoDir = repoDir ?: Path.of(System.getProperty("user.dir"))
@@ -53,6 +64,9 @@ object GenerateCommand: CliktCommand(
         generator.generateCodeOfConduct()
         generator.generateChangelog()
         generator.generateGitignore()
+        generator.generateGradleWrapper()
+        generator.generateGradleRootProject()
+        generator.generateGithubWorkflows()
         println("Done!")
     }
 
@@ -71,48 +85,147 @@ object GenerateCommand: CliktCommand(
         private val metadata: RepoMetadata,
     ) {
         fun generateReadme() {
-            generate("README.md", metadata.files.readme.generate, metadata.files.readme.update)
+            val generate = metadata.files.readme.generate
+            val update = metadata.files.readme.update || forceUpdate
+            generate("README.md", generate, update)
         }
 
         fun generateLicense() {
-            generate("LICENSE.md", metadata.files.license.generate, metadata.files.license.update)
+            val generate = metadata.files.license.generate
+            val update = metadata.files.license.update || forceUpdate
+            generate("LICENSE.md", generate, update)
         }
 
         fun generateContributing() {
-            generate("CONTRIBUTING.md", metadata.files.contributing.generate, metadata.files.contributing.update)
+            val generate = metadata.files.contributing.generate
+            val update = metadata.files.contributing.update || forceUpdate
+            generate("CONTRIBUTING.md", generate, update)
         }
 
         fun generateCodeOfConduct() {
-            generate("CODE_OF_CONDUCT.md", metadata.files.codeOfConduct.generate, metadata.files.codeOfConduct.update)
+            val generate = metadata.files.codeOfConduct.generate
+            val update = metadata.files.codeOfConduct.update || forceUpdate
+            generate("CODE_OF_CONDUCT.md", generate, update)
         }
 
         fun generateChangelog() {
-            generate("CHANGELOG.md", metadata.files.changelog.generate, metadata.files.changelog.update)
+            val generate = metadata.files.changelog.generate
+            val update = metadata.files.changelog.update || forceUpdate
+            generate("CHANGELOG.md", generate, update)
         }
 
         fun generateGitignore() {
-            generate(".gitignore", metadata.files.gitignore.generate, metadata.files.gitignore.update)
+            val generate = metadata.files.gitignore.generate
+            val update = metadata.files.gitignore.update || forceUpdate
+            generate("gitignore", generate, update, path = ".gitignore")
         }
 
-        fun generate(filename: String, generate: Boolean, update: Boolean) {
-            val outputFile = repoDir.resolve(filename)
+        fun generateGradleWrapper() {
+            val generate = metadata.files.gradleWrapper.generate
+            val update = metadata.files.gradleWrapper.update || forceUpdate
+            generateGradleWrapper(generate, update)
+        }
+
+        fun generateGradleRootProject() {
+            val generate = metadata.files.gradleRootProject.generate
+            val update = metadata.files.gradleRootProject.update || forceUpdate
+            generate("settings.gradle.kts", generate, update)
+            generate("build.gradle.kts", generate, update)
+        }
+
+        fun generateGithubWorkflows() {
+            val generate = metadata.files.githubWorkflows.generate
+            val update = metadata.files.githubWorkflows.update || forceUpdate
+            generate("github/workflows/build.yaml", generate, update, path = ".github/workflows/build.yaml")
+            if (metadata.files.githubWorkflows.buildDocs) {
+                generate("github/workflows/documentation.yaml", generate, update, path = ".github/workflows/documentation.yaml")
+            }
+        }
+
+        private fun generate(templateName: String, generate: Boolean, update: Boolean, path: String = templateName) {
+            val outputFile = repoDir.resolve(path)
             val outputFileExisted = outputFile.exists()
             if (!outputFileExisted && !generate) {
-                println("$filename: Not generated")
+                println("$path: Not generated")
                 return
             } else if (outputFileExisted && !update) {
-                println("$filename: Not updated")
+                println("$path: Not updated")
                 return
             }
 
             FileOutput(outputFile).use { output ->
-                engine.render("$filename.kte", metadata, output)
+                engine.render("$templateName.kte", metadata, output)
             }
 
             if (!outputFileExisted) {
-                println("$filename: Generated")
+                println("$path: Generated")
             } else {
-                println("$filename: Updated")
+                println("$path: Updated")
+            }
+        }
+
+        @OptIn(ExperimentalPathApi::class)
+        private fun generateGradleWrapper(generate: Boolean, update: Boolean) {
+            // We use the gradle/wrapper/gradle-wrapper.properties file to determine whether a Gradle wrapper
+            //  is present and configured, as this is the file that might be customized by the user. The other
+            //  files can safely be regenerated.
+            val gradleWrapperProperties = repoDir.resolve("gradle/wrapper/gradle-wrapper.properties")
+            val gradleWrapperPropertiesExisted = gradleWrapperProperties.exists()
+            if (!gradleWrapperPropertiesExisted && !generate) {
+                println("Gradle wrapper: Not generated")
+                return
+            } else if (gradleWrapperPropertiesExisted && !update) {
+                println("Gradle wrapper: Not updated")
+                return
+            }
+
+            // Generate the wrapper in a temporary directory and copy it to the repository
+            val tmpDir = createTempDirectory()
+            val settingsFile = tmpDir.resolve("settings.gradle.kts")
+            settingsFile.createFile()
+            val processBuilder = ProcessBuilder().apply {
+                command(
+                    gradleBin,
+                    "wrapper",
+                    "--gradle-version=${metadata.files.gradleWrapper.gradleVersion}",
+                    "--distribution-type=${metadata.files.gradleWrapper.gradleDistributionType}",
+                    "--quiet",
+                )
+                directory(tmpDir.toFile())
+                // Merge STDERR into STDOUT
+                redirectErrorStream(true)
+            }
+            try {
+                // THROWS: IOException, SecurityException, UnsupportedOperationException
+                val process = processBuilder.start()
+                // NOTE: We don't close streams that we didn't open.
+                val stdout = process.inputStream.bufferedReader().readText()
+                // THROWS: InterruptedException
+                val exitCode = process.waitFor()
+                if (exitCode != 0) throw IOException(stdout.trim())
+            } catch (ex: IOException) {
+                println("Gradle wrapper: Failed to generate: ${ex.message}")
+                return
+            } catch (ex: SecurityException) {
+                println("Gradle wrapper: Failed to generate: ${ex.message}")
+                return
+            } catch (ex: UnsupportedOperationException) {
+                println("Gradle wrapper: Failed to generate: ${ex.message}")
+                return
+            } catch (ex: InterruptedException) {
+                println("Gradle wrapper: Failed to generate: ${ex.message}")
+                return
+            }
+
+            // Remove the temporary Gradle settings file
+            settingsFile.deleteExisting()
+            // Copy the generated files back to the repository directory, overwriting what's there
+            tmpDir.copyToRecursively(repoDir, followLinks = false, overwrite = true)
+
+            if (!gradleWrapperPropertiesExisted) {
+                println("Gradle wrapper: Generated")
+            } else {
+                println("Gradle wrapper: Updated")
             }
         }
     }
